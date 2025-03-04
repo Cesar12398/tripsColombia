a6bea78210c8c550ed8da2617256c8b1cefcbe3d
Cesar12398 <merino12398@gmail.com>
2025-03-04 16:01:18 +0100

    casa

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	include 'conexion.php';
+include 'conexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,8 +13,13 @@
 </head>
 <style>
 	.container-fluid {
-		background-color: #C6C600 ;
+		background-color: #FFCD00 ;
 	}
+
+	.btn{
+		background-color: #003087;
+	}
+
 </style>
 <body>
 	<div class="container-fluid p-1">
@@ -23,19 +28,30 @@
 
 	<div class="container mt-3 col-md-3 shadow p-2 mb-3">
 		<h3 class="text-center">Registro de Gastos</h3>
-		 <form action="procesar.php" method="POST">
-            <label class="form-label" for="gasto">Gasto:</label>
-            <input type="hidden" id="accion" name="accion" value="agregar">
-            <input type="text" class="form-control" id="gasto" name="gasto" placeholder="Ingrese Gasto" required><br>
-            <label for="telefono">Monto:</label>
-            <input type="text" class="form-control" id="monto" name="monto" placeholder="Ingrese Monto" required><br>
-            <button type="submit" class="btn btn-primary">Guardar</button>
-        </form>
+		<form action="procesar.php" method="POST">
+			<label class="form-label" for="gasto">Gasto:</label>
+			<input type="hidden" id="accion" name="accion" value="agregar">
+			<input type="text" class="form-control" id="gasto" name="gasto" placeholder="Ingrese Gasto" required><br>
+			<label for="telefono">Monto:</label>
+			<input type="text" class="form-control" id="monto" name="monto" placeholder="Ingrese Monto" required><br>
+			<button type="submit" class="btn text-white">Guardar</button>
+		</form>
 
 	</div>
 
+		<div class="container mt-3 table-responsive">
+			<table class="table table-hover text-center">
+				<thead class="">
+					<tr>
+						<th>ID</th>
+						<th>GASTO</th>
+						<th>BASE</th>
+						<th>IVA (21%)</th>
+						<th>MONTO</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
 
-
-	
 </body>
 </html>
